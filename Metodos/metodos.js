5add88f09c95de8967b8488ca0b4e34e5ee6ba72
josepoMP <jerosoto2410@gmail.com>
2025-09-09 11:53:44 -0500

    creacion de archivos

---
+++ Metodos/metodos.js
@@ -0,0 +1,10 @@
+//Creacion de una funcion
+function imprimirMensaje(message) {
+    console.log(message);
+}
+imprimirMensaje("holiiiiiiiiiiii");
+function sumar(numero1, numero2) {
+    return numero1 + numero2;
+}
+var resultado = sumar(50, 35);
+console.log("Tu resultado es:", resultado);
---
+++ Metodos/metodos.ts
@@ -0,0 +1,14 @@
+//Creacion de una funcion
+
+function imprimirMensaje(message:string): void{
+    console.log(message);
+     
+}
+imprimirMensaje("holiiiiiiiiiiii")
+
+
+function sumar(numero1:number, numero2:number):number{
+    return numero1+numero2;
+}
+let resultado:number=sumar(50,35);
+console.log("Tu resultado es:", resultado);
